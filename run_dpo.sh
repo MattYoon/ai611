01f30a1357ee4a52cdd647b0589cf58ff4c6fbc9
MattYoon <mattyoon99@gmail.com>
2024-11-21 15:11:13 +0900

    .

--- run_dpo.sh
+++ run_dpo.sh
@@ -1,3 +1,5 @@
 trl dpo --model_name_or_path google/gemma-2-2b-it \
     --dataset_name Joocjun/seahorse4dpo \
-    --output_dir gemma-2-2b-seahorse4dpo
+    --output_dir gemma-2-2b-seahorse4dpo \
+    --torch_dtype torch.bfloat16 \
+    --report_to wandb \
