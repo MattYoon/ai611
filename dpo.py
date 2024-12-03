3818eeddd95e286bc330fe9f64d7c336c898814e
MattYoon <mattyoon99@gmail.com>
2024-12-03 15:48:00 +0900

    added bloomz scripts

--- dpo.py
+++ dpo.py
@@ -68,7 +68,8 @@ if __name__ == "__main__":
     parser = TrlParser((ScriptArguments, DPOConfig, ModelConfig))
     script_args, training_args, model_config = parser.parse_args_and_config()
 
-    training_args.optim = 'paged_adamw_8bit'
+    # training_args.optim = 'paged_adamw_8bit'
+    training_args.optim = 'adamw_torch_fused'
     training_args.save_only_model = True
 
     ################
---
+++ run_dpo2.sh
@@ -0,0 +1,17 @@
+python dpo.py \
+    --dataset_name Joocjun/seahorse4dpo \
+    --model_name_or_path bigscience/bloomz-1b7 \
+    --learning_rate 5e-7 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --gradient_checkpointing \
+    --logging_steps 25 \
+    --eval_strategy no \
+    --output_dir checkpoints/bloomz-1b7-seahorse4dpo \
+    --no_remove_unused_columns \
+    --bf16 true \
+    --use_liger_kernel \
+    --report_to wandb \
+    --run_name bloomz-1b7-seahorse4dpo \
+    --max_length 2048 \
